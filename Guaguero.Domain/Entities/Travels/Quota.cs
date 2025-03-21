0272ee5dec2305fa2c1f361c623214183fa5f322
CristXanderAza <cristopherxanderazadiaz19111@gmail.com>
2025-03-21 11:05:41 -0400

    Cambio de Quota a PK compuesta

--- Guaguero.Domain/Entities/Travels/Quota.cs
+++ Guaguero.Domain/Entities/Travels/Quota.cs
@@ -6,17 +6,17 @@ namespace Guaguero.Domain.Entities.Travels
 {
     public class Quota : AuditEntity
     {
-        public Guid QuotaID { get; set; }
+       // public Guid QuotaID { get; set; }
         public Guid TravelID { get; set; }
-        public Travel Travel { get; set; }
+        public virtual Travel Travel { get; set; }
         public Guid CustomerID { get; set; }
-        public Customer Customer { get; set; }
+        public virtual Customer Customer { get; set; }
         public Guid ArrivalStopID { get; set; }
-        public TravelStop ArrivalStop { get; set; }
+        public virtual TravelStop ArrivalStop { get; set; }
         public Guid DepertureStopID { get; set; }
-        public TravelStop DepertureStop { get; set; }
+        public virtual TravelStop DepertureStop { get; set; }
         public Guid PaymentID { get; set; }
-        public PaymentBase Payment { get; set; }
+        public virtual PaymentBase Payment { get; set; }
         public QuotaState Status { get; set; }
     }
 }
